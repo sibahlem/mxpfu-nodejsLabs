fc9fb6fe77b46226049b956903f5395b52b51ee5
sibahlem <sibahle.molphy@gmail.com>
2024-03-26 21:33:43 -0400

    changes from thia virtual env

--- routes/users.js
+++ routes/users.js
@@ -25,22 +25,23 @@ let users = [
 
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
-});
+    res.send(users);
+}); // You will first add an API endpoint, using the get method for getting the details of all users.
 
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    const email = req.params.email;
+    let filtered_users = users.filter((user) => user.email === email);
+    res.send(filtered_users); //Implement a get method for getting the details of a specific user based on their email ID by using the filter method on the user collection.
+    // 'localhost:5000/user/johnsmith@gamil.com' to retrieve a specific user w their email.
 });
 
 
 // POST request: Create a new user
 router.post("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
-});
+    users.push({"firstName":req.query.firstName,"lastName":req.query.lastName,"email":req.query.email,"DOB":req.query.DOB});
+    res.send("The user" + (' ')+ (req.query.firstName) + " Has been added!")
+}); // We've implemented the /user endpoint with the POST method to create a user and add the user to the list. We created the user object as a dictionary.
 
 
 // PUT request: Update the details of a user by email ID
