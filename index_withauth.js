9cf37d879ab2d8f00ea357dad5227cae70f37a1c
Sibahle <60863779+sibahlem@users.noreply.github.com>
2024-03-28 02:08:31 +0200

    Update index_withauth.js

--- index_withauth.js
+++ index_withauth.js
@@ -6,12 +6,15 @@ const session = require('express-session')
 const app = express();
 const PORT =5000;
 
-app.use(session({secret:"fingerpint",resave: true, saveUninitialized: true}))
+app.use(session({secret:"fingerpint",resave: true, saveUninitialized: true})) //This tells your express app to use the session middleware.
+/* secret - a random unique string key used to authenticate a session.
+resave - takes a Boolean value. It enables the session to be stored back to the session store, even if the session was never modified during the request.
+saveUninitialized - this allows any uninitialized session to be sent to the store. When a session is created but not modified, it is referred to as uninitialized.*/
 
 app.use(express.json());
 
 app.use("/user", (req,res,next)=>{
-// Middleware which tells that the user is authenticated or not
+// Authentication Middleware: tells if the user is authenticated or not.
    
    if(req.session.authorization) {
        let token = req.session.authorization['accessToken']; // Access Token
@@ -28,11 +31,14 @@ app.use("/user", (req,res,next)=>{
     } else {
         return res.status(403).json({message: "User not logged in"})
     }
-});
+}); /*All the endpoints starting with /user will go through this middleware.
+    It will retrieve the authorization details from the session and verify it.
+    If the token is validated, the user is authenticated and the control is passed on to the next endpoint handler.
+    If the token is invalid, the user is not authenticated and an error message is returned.*/
 
 app.use("/user", routes);
 
-app.post("/login", (req,res) => {
+app.post("/login", (req,res) => { // Implementation of the login endpoint.
     const user = req.body.user;
     if (!user) {
         return res.status(404).json({message: "Body Empty"});
@@ -45,6 +51,9 @@ app.post("/login", (req,res) => {
         accessToken
     }
     return res.status(200).send("User successfully logged in");
-});
+}); /*  A user logs into the system providing a username.
+    An access token that is valid for one hour is generated.
+    You may observe this validty length specified by 60 * 60, which signifies the time in seconds.
+    This access token is set into the session object to ensure that only authenticated users can access the endpoints for that length of time.*/
 
 app.listen(PORT,()=>console.log("Server is running at port "+PORT));
